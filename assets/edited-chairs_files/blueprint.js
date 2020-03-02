644a0fdebc6ead7c003ffa7b33b59bfe041414f9
Andro <andro.miocevic@gmail.com>
2020-03-02 16:41:54 +0100

    Fixed background

---
+++ assets/edited-chairs_files/blueprint.js
@@ -0,0 +1,284 @@
+window.blueprint = {
+	"width": 250,
+	"height": 250,
+	"images": [
+		"logo_vimeo.png",
+		"logo_reelboy.png",
+		"txt_1.png",
+		"txt_2.png",
+		"txt_3.png",
+		"cta_1.png",
+		"ctaHover_1.png",
+		"hand.png"
+	],
+	"elements": {
+		"stage": {
+			"parent": "banner",
+			"id": "stage",
+			"retina": false,
+			"left": 0,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 250,
+			"height": 250
+		},
+		"bg": {
+			"parent": "banner",
+			"id": "bg",
+			"retina": false,
+			"left": 0,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 250,
+			"height": 250
+		},
+		"logo_vimeo": {
+			"parent": "banner",
+			"id": "logo_vimeo",
+			"retina": true,
+			"left": 20,
+			"top": 200,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"backgroundImage": "logo_vimeo.png",
+			"width": 80,
+			"height": 23
+		},
+		"logo_reelboy": {
+			"parent": "banner",
+			"id": "logo_reelboy",
+			"retina": true,
+			"left": 23,
+			"top": 179,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"backgroundImage": "logo_reelboy.png",
+			"width": 54,
+			"height": 46
+		},
+		"txt_1": {
+			"parent": "banner",
+			"id": "txt_1",
+			"retina": true,
+			"left": -7,
+			"top": -7,
+			"safeArea": 28,
+			"lineHeight": 30.800000000000004,
+			"backgroundImage": "txt_1.png",
+			"width": 254,
+			"height": 111
+		},
+		"txt_2": {
+			"parent": "banner",
+			"id": "txt_2",
+			"retina": true,
+			"left": -18,
+			"top": 24.300000000000004,
+			"safeArea": 39,
+			"lineHeight": 29.924999999999997,
+			"backgroundImage": "txt_2.png",
+			"width": 284,
+			"height": 135
+		},
+		"txt_3": {
+			"parent": "banner",
+			"id": "txt_3",
+			"retina": true,
+			"left": 5,
+			"top": 115,
+			"safeArea": 20,
+			"lineHeight": 20.8,
+			"backgroundImage": "txt_3.png",
+			"width": 197,
+			"height": 57
+		},
+		"cta": {
+			"parent": "banner",
+			"id": "cta",
+			"retina": true,
+			"left": 122,
+			"top": 200.54999999999998,
+			"safeArea": 0,
+			"lineHeight": 13.52
+		},
+		"cta_1": {
+			"parent": "cta",
+			"id": "cta_1",
+			"retina": true,
+			"backgroundImage": "cta_1.png"
+		},
+		"ctaHover": {
+			"parent": "banner",
+			"id": "ctaHover",
+			"retina": true,
+			"left": 144,
+			"top": 36.550000000000004,
+			"safeArea": 0,
+			"lineHeight": 13.52
+		},
+		"ctaHover_1": {
+			"parent": "ctaHover",
+			"id": "ctaHover_1",
+			"retina": true,
+			"backgroundImage": "ctaHover_1.png"
+		},
+		"logo_reelboy_pos3": {
+			"parent": "banner",
+			"id": "logo_reelboy_pos3",
+			"retina": false,
+			"left": 23,
+			"top": 299,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 54,
+			"height": 46
+		},
+		"logo_reelboy_pos2": {
+			"parent": "banner",
+			"id": "logo_reelboy_pos2",
+			"retina": false,
+			"left": 23,
+			"top": 179,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 54,
+			"height": 46
+		},
+		"logo_reelboy_pos1": {
+			"parent": "banner",
+			"id": "logo_reelboy_pos1",
+			"retina": false,
+			"left": 23,
+			"top": 269,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 54,
+			"height": 46
+		},
+		"logo_vimeo_pos1": {
+			"parent": "banner",
+			"id": "logo_vimeo_pos1",
+			"retina": false,
+			"left": 20,
+			"top": 201,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 80,
+			"height": 24
+		},
+		"logo_vimeo_pos2": {
+			"parent": "banner",
+			"id": "logo_vimeo_pos2",
+			"retina": false,
+			"left": -90,
+			"top": 231,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 80,
+			"height": 24
+		},
+		"hand_pos2": {
+			"parent": "banner",
+			"id": "hand_pos2",
+			"retina": false,
+			"left": 90,
+			"top": 166,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 43,
+			"height": 53
+		},
+		"hand_pos1": {
+			"parent": "banner",
+			"id": "hand_pos1",
+			"retina": false,
+			"left": 94,
+			"top": 258,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 44,
+			"height": 53
+		},
+		"hand": {
+			"parent": "banner",
+			"id": "hand",
+			"retina": true,
+			"left": 164,
+			"top": 116,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"backgroundImage": "hand.png",
+			"width": 43,
+			"height": 52
+		},
+		"top": {
+			"parent": "banner",
+			"id": "top",
+			"retina": false,
+			"left": 0,
+			"top": -10,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 250,
+			"height": 10
+		},
+		"bottom": {
+			"parent": "banner",
+			"id": "bottom",
+			"retina": false,
+			"left": 0,
+			"top": 250,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 250,
+			"height": 10
+		},
+		"left": {
+			"parent": "banner",
+			"id": "left",
+			"retina": false,
+			"left": -10,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 10,
+			"height": 250
+		},
+		"right": {
+			"parent": "banner",
+			"id": "right",
+			"retina": false,
+			"left": 250,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 10,
+			"height": 250
+		}
+	},
+	"settings": {
+		"filename": "Q3-2019_EN_mid-funnel_videopro_custom-logo_2_learn-more_250x250_HTML5",
+		"market": "EN",
+		"campaign": "Q3-2019",
+		"funnel": "mid-funnel",
+		"segment": "videopro",
+		"feature": "custom-logo",
+		"copyVariation": "2",
+		"size": "250x250",
+		"marketIndex": "1",
+		"id": "videoprocustom-logo2",
+		"copy": {
+			"copy1": "Tell our logo to go-go.",
+			"copy2": "Customizable Player.",
+			"copy3": "A Vimeo Feature",
+			"copy4": "Learn More",
+			"copy5": ""
+		},
+		"fileType": "jpg",
+		"fallback": true,
+		"HtmlfileType": "html",
+		"backgroundColor": "#e59fb9"
+	}
+};
